6daac1e413a6c3c56d3cfee7a82bc25723423f6c
Tommaso Iovane <tommaso.iovane@gmail.com>
2017-10-19 15:47:25 +0200

    sec

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const request = require('request')
 const GoogleImages = require('google-images')
 
 const google = new GoogleImages('000402205720418894190:gkqwcqc1lau', 'AIzaSyDbUxf_K0K0Q0aDWq9iLHYH0LflJu7SaDs')
-
+const api = 'api_key=RGAPI-b5526745-c829-4869-9103-67d73c5043fb'
 let champions 
 
-request('https://euw1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/20673206?api_key=RGAPI-e2c2ebed-b75a-4020-b090-51e0ecf9cfc9', function(error, response, body) {
+request('https://euw1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/20673206?' + api + '', function(error, response, body) {
     champions = body
 })
 
@@ -23,13 +23,15 @@ express()
         let go = 0
         
 
-        request('https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' +name+ '?api_key=RGAPI-e2c2ebed-b75a-4020-b090-51e0ecf9cfc9', function(error, response, body) {
+        request('https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' +name+ '?' + api + '', function(error, response, body) {
             console.log(body)
             
             let player = JSON.parse(body)
-
+            console.log(name)
             res.render('result', {
                 player,
+                title: 'profile',
+                id: player.accountId
             })
 
         })
@@ -37,14 +39,41 @@ express()
 
     })
 
-    .get('/img', (req, res) => {
-        google.search('akalisquare')
+    .get('/img/:src', (req, res) => {
+        let image = req.params.src
+        google.search(image+'square')
             .then((img) => {
                 res.send(img)
                 if(img[0].width == 120){
                     console.log('ok')
                 }
             })
+    })
+
+    .get('/match/:id', (req, res) => {
+        let playerId = req.params.id
+
+        request('https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/' + playerId + '?' + api, function(error, response, body){
+            let parsed = JSON.parse(body)
+            console.log(parsed.matches)
+            let match = parsed.matches
+            res.render('match', {
+                match
+            })
+        })
+    })
+
+    .get('/gameInfo/:gameId', (req, res) => {
+        let gameId = req.params.gameId
+
+        request('https://euw1.api.riotgames.com/lol/match/v3/matches/'+ gameId + '?' + api, function(error, response,body){
+            let parsed = JSON.parse(body)
+            let teams = parsed.teams
+            res.render('game', {
+                parsed,
+                teams
+            })
+        })
     })
 
     .listen(3300)
